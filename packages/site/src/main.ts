6f483ea295253ece3d783d8107b97d8340c4f8a8
iwander <2317791932@qq.com>
2022-08-18 22:16:58 +0800

    feat(site): add reboot scss style

--- packages/site/src/main.ts
+++ packages/site/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import router from './router/index.js';
 import App from './App.vue';
 
+import './style/index.scss';
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
